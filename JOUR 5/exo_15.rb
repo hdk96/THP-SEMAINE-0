ccca36c627b19d2af458bd84456394f065c894f6
hdk <amir.madi34@gmail.com>
2018-06-25 21:47:08 +0000

    correcting exo_16.rb

--- JOUR 5/exo_15.rb
+++ JOUR 5/exo_15.rb
@@ -1,15 +1,13 @@
 puts "Bonjour entre ton année de naissance"
 
-user_birth_year = gets.chomp
+birth_year= gets.chomp.to_i
 
-birth_year = Integer(user_birth_year)
+moving_year = birth_year
 
 for i in birth_year..2017
 
-
-    puts "tu a eu #{birth_year - 1996 } ans en #{birth_year} "
+    puts "tu avais  #{moving_year-birth_year} ans  en #{moving_year}   "
     
-    birth_year +=1
+    moving_year +=1
     
-    
-end 
+end
