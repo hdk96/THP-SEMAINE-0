de4b2b51d660f1f9f345ef29da39f25b99006627
hdk <amir.madi34@gmail.com>
2018-06-26 14:54:11 +0000

    correction exo_17.rb

--- JOUR 5/exo_17.rb
+++ JOUR 5/exo_17.rb
@@ -1,22 +1,17 @@
-puts "Bonjour entre ton année de naissance"
+puts "Entre ton age!"
 
-user_birth_year = gets.chomp
+user_age= gets.chomp.to_i
 
-birth_year = Integer(user_birth_year)
-fixed_year = Integer(user_birth_year)
-for i in birth_year..2017
+for i in 0..user_age
+   if i == user_age
+       
+       puts "il y'a #{i} ans tu avais la moitié de l'age que tu as aujourd'hui "
+       
+   else
+       
+    puts "tu avais #{i} ans il y'a #{user_age} ans"
+   end
+   user_age -= 1
+end
 
-    if 2017-birth_year == birth_year - fixed_year
-        puts "il y'a #{2017-birth_year}  tu avais la moitié de l'age que tu as aujourd'hui "
-        #cela marche uniquement si l'utilisateur a un age pair  en 2017
-    else
-        
-        puts "Il y'a #{2017-birth_year} ans  tu avais #{birth_year - fixed_year } ans "
-    
-    end
-        
-        
-    birth_year +=1
-    
-    
-end 
+
